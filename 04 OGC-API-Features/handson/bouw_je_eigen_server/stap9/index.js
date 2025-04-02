00674c2f0142b6ae644023ed29e1d4a0ce8cd2da
Niels Hoffmann <n.hoffmann@geonovum.nl>
2025-04-02 16:46:50 +0200

    OGC API Features Client voorbeelden

--- 04 OGC-API-Features/handson/bouw_je_eigen_server/stap9/index.js
+++ 04 OGC-API-Features/handson/bouw_je_eigen_server/stap9/index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const port = 8080
 
 var amstelveen = require('./amstelveen')
+
+app.use(cors(
+    {
+        origin: 'http://127.0.0.1:5500', // Only allow requests from this origin
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow these HTTP methods
+        credentials: true, // Allow cookies to be sent
+      }
+)); // Enable CORS for all routes
 
 app.use('/demoservice/v1', amstelveen)
 
---
+++ 04 OGC-API-Features/handson/ogc-api-features-webclient/leaflet/readme.md
@@ -0,0 +1,3 @@
+# Leaflet.js demo
+
+De Leaflet demo gebruikt de NAP Peilmerken uit de NodeJS OGC API Features Server van bouw_je_eigen_server
